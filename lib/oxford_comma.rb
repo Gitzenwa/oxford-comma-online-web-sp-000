5f774d6581fb755b40bbae73aa1c0807d242e437
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-31 03:13:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    array.join(" ")
+    array.join(" and  ")
   elsif array.length == 3
     array.insert(1, ",")
     array.insert(2, " and ")
